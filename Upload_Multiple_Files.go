9562967c7cb548a55ad2d8ca21212f953f19b23a
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-11-18 15:28:38 +0330

    multiple files

---
+++ Upload_Multiple_Files.go
@@ -0,0 +1,19 @@
+// Upload multiple files
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Router() {
+	router.POST(&quot/upload&quot, func(c *gin.Context) {
+		form, _ := c.MultipartForm()
+		files := form.File[&quotupload[]&quot]
+		for _, file := range files {
+		c.SaveUploadedFile(file, dst)
+		}
+		})
+	})
+}
