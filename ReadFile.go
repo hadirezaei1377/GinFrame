bcb43f0754980ea264b20f5654922ac0b221b817
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-11-18 15:20:06 +0330

    Multipart in Gin

---
+++ ReadFile.go
@@ -0,0 +1,10 @@
+package main
+
+// We have a form called form.html.
+// and a text box named message.
+// We are going to send the information from this form to the server.
+router.POST(&quot/form_post&quot, func(c *gin.Context) {
+	// c.PostForm is main command for this task
+	message := c.PostForm(&quotmessage&quot)
+	})
+	
